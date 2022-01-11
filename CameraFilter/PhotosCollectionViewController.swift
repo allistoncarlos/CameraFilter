7094d20e70adf662c5cdd00911cea3ed8065abfb
Alliston <alliston@outlook.com>
2022-01-11 14:46:30 -0300

    Creating UI structure and pod init/install

---
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosCollectionViewController.swift
+//  CameraFilter
+//
+//  Created by Alliston Aleixo on 11/01/22.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+  
+}
