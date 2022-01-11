101df20252219bb570fdd14c0e025f47ea804b76
Alliston <alliston@outlook.com>
2022-01-11 12:49:57 -0300

    Initial Commit

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Alliston Aleixo on 11/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
