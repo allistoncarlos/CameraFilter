4bf9df76660a5c3550d3a148c24734867e419b0b
Alliston <alliston@outlook.com>
2022-01-13 14:03:07 -0300

    Asking for photos permission

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -6,7 +6,20 @@
 //
 
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    populatePhotos()
+  }
   
+  private func populatePhotos() {
+    PHPhotoLibrary.requestAuthorization { status in
+      if status == .authorized {
+        
+      }
+    }
+  }
 }
